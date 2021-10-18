f797c8699f1171027a5560ce65bc7005b269203c
hayato-lang <83726781+hayato-lang@users.noreply.github.com>
2021-10-19 08:45:29 +0900

    クエリビルダ

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Models\Test;
 
+use Illuminate\Support\Facades\DB;
+
 class TestController extends Controller
 {
     //
@@ -13,13 +15,17 @@ class TestController extends Controller
     {
         $values = Test::all();
 
+        
+
         $collection = collect([1,2,3,4,5,6,7]);
 
         $chunks = $collection->chunk(4);
 
         $chunks->toArray();
 
-        dd($chunks);
+        $tests = DB::table('tests')->get();
+
+        dd($tests);
 
         return view('tests.test', compact('values'));
     }
